0942520e5eafe6ed4b3b7ea6728556f0efa214a4
Jake Wood <jzwood14@yahoo.com>
2021-11-09 20:46:39 -0500

    more tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -3,10 +3,39 @@ module ParserSpec (spec) where
 import Test.Hspec
 import Parser
 
+success :: Either Cursor (a, Cursor, String) -> Maybe (a, String)
+success (Left _) = Nothing
+success (Right (a, _, b)) = Just (a, b)
+
 spec :: Spec
 spec = do
   describe "Parser" $ do
-    it "parses parens" $ do
-      let result = runParser camel mempty "cat"
-          expected = Right ("cat",Cursor {line = 0, col = 3},"")
-      result `shouldBe` expected
+    it "camel" $ do
+      let result = runParser camel mempty "catWorld"
+      success result `shouldBe` Just ("catWorld", "")
+      let result = runParser camel mempty "Cat"
+      success result `shouldBe` Nothing
+
+    it "pascal" $ do
+      let result = runParser pascal mempty "CatWorld"
+      success result `shouldBe` Just ("CatWorld", "")
+      let result = runParser pascal mempty "catWorld"
+      success result `shouldBe` Nothing
+
+    it "trim" $ do
+      let result = runParser (trim camel) mempty "  cat  "
+      success result `shouldBe` Just ("cat", "")
+
+    it "paren" $ do
+      let result = runParser (paren camel) mempty "(cat)"
+      success result `shouldBe` Just ("cat", "")
+      let result = runParser (paren camel) mempty "(cat"
+      success result `shouldBe` Nothing
+      let result = runParser (paren . trim $ camel) mempty "(  cat  )"
+      success result `shouldBe` Just ("cat", "")
+
+    it "numbers" $ do
+      let result = runParser integer mempty "123abc"
+      success result `shouldBe` Just (123, "abc")
+      let result = runParser number mempty "123.456abc"
+      success result `shouldBe` Just (123.456, "abc")
