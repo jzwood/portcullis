64ef0de92f5faa1d20569df0ec1b71676c5253a1
Jake Wood <jzwood14@yahoo.com>
2021-11-20 16:55:50 -0500

    first typechecker tests pass !!!!

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -28,8 +28,8 @@ typecheck :: TypeExpr -> [Expr] -> Either TypeError TypeExpr
 typecheck t [] = Right t
 typecheck (NumType _) _ = Left WrongArity -- we cannot apply expr(s) to numtype
 typecheck (Arrow t1 t2) (e:es) =
-  typeofExpr e <&>  -- get type of expression
-  typeCompare t1 >>  -- compare type with sig
+  typeofExpr e >>=  -- get type of expression
+  typeCompare t1 >>  -- compare type with sig (
   typecheck t2 es  -- typecheck tail of sig against tail of expression
 
 --typecheck :: Map Name TypeExpr -> Stmt -> Bool
@@ -40,27 +40,31 @@ typecheck (Arrow t1 t2) (e:es) =
   --then Right (NumType "Num")
   --else Left "bin expresssions are wrong type"
 
-typeCompare :: TypeExpr -> TypeExpr -> Bool
-typeCompare = (==)
+typeCompare :: TypeExpr -> TypeExpr -> Either TypeError TypeExpr
+typeCompare t1 t2
+  | t1 == t2 = Right t2
+  | otherwise = Left Mismatch
 
 typeofExpr :: Expr -> Either TypeError TypeExpr
 typeofExpr (Number n) = Right $ NumType "Num"
 typeofExpr (Ident name) = undefined
 typeofExpr (Call name exprs) = undefined
 typeofExpr (Guard exprPairs) = undefined
---typeofExpr (BinOp bop expr1 expr2) = (typeCompare $ typeofBop bop) (Arrow expr1, expr2] -- typecheckBinOp bop expr1 expr2
+typeofExpr (BinOp bop expr1 expr2) = typecheck (typeofBop bop) [expr1, expr2]
 
 typeofBop :: Bop -> TypeExpr
 typeofBop bop =
   case bop of
-    Plus -> numnumnum
-    Minus -> numnumnum
-    Divide -> numnumnum
-    Times -> numnumnum
-    Mod -> numnumnum
-    Equal -> numnumbool
-    NotEqual -> numnumbool
+    Plus -> nnn
+    Minus -> nnn
+    Divide -> nnn
+    Times -> nnn
+    Mod -> nnn
+    Equal -> nnb
+    NotEqual -> nnb
+    GreaterThan -> nnb
+    LessThan -> nnb
   where
-    numnumnum = Arrow (NumType "Num") (Arrow (NumType "Num") (NumType "Num"))
-    numnumbool = Arrow (NumType "Num") (Arrow (NumType "Num") (NumType "Bool"))
+    nnn = Arrow (NumType "Num") (Arrow (NumType "Num") (NumType "Num"))
+    nnb = Arrow (NumType "Num") (Arrow (NumType "Num") (NumType "Bool"))
 
--- test/TypeCheckSpec.hs
+++ test/TypeCheckSpec.hs
@@ -7,11 +7,11 @@ import TypeChecker
 spec :: Spec
 spec = do
   describe "typecheck" $ do
-    it "a" $ do
+    it "typeofExpr (binary ops)" $ do
       let binop = BinOp Plus (Number 1) (Number 2)
-      typecheckExpr binop `shouldBe` Right (NumType "Num")
+      typeofExpr binop `shouldBe` Right (NumType "Num")
       let binop = BinOp Plus (BinOp Minus (Number 4) (BinOp Times (Number 1) (Number 5))) (Number 2)
-      typecheckExpr binop `shouldBe` Right (NumType "Num")
+      typeofExpr binop `shouldBe` Right (NumType "Num")
       let binop = BinOp GreaterThan (BinOp Minus (Number 4) (BinOp Times (Number 1) (Number 5))) (Number 2)
-      typecheckExpr binop `shouldBe` Right (NumType "Bool")
+      typeofExpr binop `shouldBe` Right (NumType "Bool")
 
