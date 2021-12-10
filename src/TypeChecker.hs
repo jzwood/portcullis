5040abbb275b8ed6c541fa7709f593d804d4b01a
Jake Wood <jzwood14@yahoo.com>
2021-12-09 22:42:13 -0500

    ok now maybe it works. still havent tested yet

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -4,12 +4,12 @@ module TypeChecker where
 
 import Data.Function
 import Data.Functor
-import qualified Data.Map as Map
-import Data.Map (Map)
-import Data.Traversable
 import Data.List
+import Data.Map (Map)
+import Data.Maybe
+import Data.Traversable
 import Syntax
-
+import qualified Data.Map as Map
 
 data TypeError
   = NotFunction
@@ -20,30 +20,24 @@ data TypeError
   -- | NotFunction TypeExpr
   -- | NotInScope Name
 
-typecheckExpr :: Expr -> TypeExpr -> Either TypeError TypeExpr
-typecheckExpr _ NumType = Left NotFunction -- we cannot apply expr(s) to numtype
-typecheckExpr _ CharType = Left NotFunction -- we cannot apply expr(s) to numtype
-typecheckExpr _ AtomType = Left NotFunction -- we cannot apply expr(s) to numtype
-typecheckExpr expr (Arrow t0 t1)
-  = typeofExpr expr -- get type of expression
-  >>= flip (typecheck t0) Map.empty  -- compare type with sig
-  <&> applyTypeMap t1
+typecheckExpr :: TypeExpr -> TypeExpr -> Either TypeError TypeExpr
+typecheckExpr t (Arrow tl tr)
+  = typecheck t tl Map.empty  -- compare type with sig
+  <&> applyTypeMap tr
+typecheckExpr _ _ = Left NotFunction -- we cannot apply expr(s) any non-arrow type
 
-typecheck :: TypeExpr -> TypeExpr -> Map Name Name -> Either TypeError (Map Name Name)
-typecheck (Unspecfied a) (Unspecfied b) m =
-  case Map.lookup a m of
-    Nothing -> Right $ Map.insert a b m
-    Just b -> if a == b then Right m else Left Mismatch
-typecheck (Arrow t1 t2) (Arrow t3 t4) m = typecheck t1 t3 m >>= typecheck t2 t3
+typecheck :: TypeExpr -> TypeExpr -> Map Name TypeExpr -> Either TypeError (Map Name TypeExpr)
+typecheck t (Unspecfied n) m =
+  case Map.lookup n m of
+    Nothing -> Right $ Map.insert n t m
+    Just t' -> if t == t' then Right m else Left Mismatch
+typecheck (Arrow t1l t1r) (Arrow t2l t2r) m = typecheck t1l t2l m >>= typecheck t2l t2r
 typecheck t1 t2 m =
   if t1 == t2 then Right m
               else Left Mismatch
 
-applyTypeMap :: TypeExpr -> Map Name Name -> TypeExpr
-applyTypeMap t@(Unspecfied a) m =
-  case Map.lookup a m of
-    Nothing -> t
-    Just name -> Unspecfied name
+applyTypeMap :: TypeExpr -> Map Name TypeExpr -> TypeExpr
+applyTypeMap t@(Unspecfied n) m = fromMaybe t (Map.lookup n m)
 applyTypeMap (Arrow tl tr) m = Arrow (applyTypeMap tl m) (applyTypeMap tr m)
 applyTypeMap t _ = t
 
@@ -66,10 +60,11 @@ typeofExpr (Guard exprPairs) = goodPs >> goodEs
           <&> typeofExpr
            &  sequence
           >>= \(t:ts) -> if all (==t) ts then Right t else Left Mismatch
-
 typeofExpr (BinOp bop expr1 expr2)
-  = typecheckExpr expr1 (typeofBop bop)
-  >>= typecheckExpr expr2
+  = sequence [typeofExpr expr1, typeofExpr expr2]
+  >>= \[t1, t2] ->
+    typecheckExpr t1 (typeofBop bop)
+    >>= typecheckExpr t2
 
 typeofBop :: Bop -> TypeExpr
 typeofBop bop =
