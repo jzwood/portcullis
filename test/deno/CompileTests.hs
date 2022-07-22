5e7f4921e0d3c5568f0a8789815b762d8fd55cb2
Jake Wood <jzwood14@yahoo.com>
2022-07-22 17:21:19 -0400

    curates example po files in test suite

--- test/deno/CompileTests.hs
+++ test/deno/CompileTests.hs
@@ -7,11 +7,13 @@ import System.FilePath ((</>), (<.>), replaceExtension, isExtensionOf)
 compile :: FilePath -> FilePath -> FilePath -> IO ()
 compile src dest file = runCompilation (src </> file) (dest </> replaceExtension file ".js")
 
+examplesPath = "test/deno/examples"
+
 main :: IO ()
 main = do
   createDirectoryIfMissing False "test/deno/compiled"
   runCompilation "test/deno/math.po" "test/deno/compiled/math.js"
   runCompilation "test/deno/hof.po" "test/deno/compiled/hof.js"
   -- all example portcullis programs should compile
-  examples <- filter ("po" `isExtensionOf`) <$> listDirectory "examples"
-  mapM_ (compile "examples" "test/deno/compiled") examples
+  examples <- filter ("po" `isExtensionOf`) <$> listDirectory examplesPath
+  mapM_ (compile examplesPath "test/deno/compiled") examples
--- test/deno/compiled/hof.js
+++ test/deno/compiled/hof.js
@@ -3,21 +3,30 @@ const True = 1
 
 export const one1 = $one1()
 export const one2 = $one2()
-export const one3 = $one3()
+
+// signature: (x -> x)
+export function id(x) {
+  return x;
+}
 
 // signature: ((b -> c) -> ((a -> b) -> (a -> c)))
 export function compose(f) {
   return (g) => (x) => f(g(x));
 }
 
-// signature: (x -> x)
-export function id1(x) {
-  return x;
+// signature: (Num -> Num)
+export function double(x) {
+  return (2.0 * x);
 }
 
 // signature: (Num -> Num)
+export function quadruple(n) {
+  return compose(double)(double)(n);
+}
+
+// signature: (x -> x)
 export function id2(x) {
-  return compose(id1)(id1)(x);
+  return compose(id)(id)(x);
 }
 
 // signature: Num
@@ -27,12 +36,78 @@ function $one1() {
 
 // signature: Num
 function $one2() {
-  return one1;
+  return compose(id)(id)(one1);
 }
 
-// signature: Num
-function $one3() {
-  return one1;
+// signature: (z -> ([z] -> [z]))
+export function id3(x) {
+  return (xs) => xs;
+}
+
+// signature: ([t] -> [t])
+export function tail(xs) {
+  return (
+    /* if */ equal(xs, []) ?
+    /* then */ xs :
+    /* else */ id3(xs.at(0))(xs.slice(1))
+  );
+}
+
+// signature: ((h -> h) -> (h -> h))
+export function a(fx) {
+  return (x) => fx(x);
+}
+
+// signature: (q -> q)
+export function b(w) {
+  return w;
+}
+
+// signature: (p -> p)
+export function c(y) {
+  return a(b)(y);
+}
+
+// signature: ([h] -> (h -> ([h] -> [h])))
+export function push(ys) {
+  return (x) => (xs) => [x, ...concat(xs)(ys)];
+}
+
+// signature: ([a] -> ([a] -> [a]))
+export function concat(xs) {
+  return (ys) => (
+    /* if */ equal(xs, []) ?
+    /* then */ ys :
+    /* else */ push(ys)(xs.at(0))(xs.slice(1))
+  );
+}
+
+// signature: ((x -> Atom) -> (x -> ([x] -> [x])))
+export function filter2(g) {
+  return (w) => (ws) => concat((
+    /* if */ g(w) ?
+    /* then */ /* [x] */ [w] :
+    /* else */ /* [x] */ []
+  ))(filter(g)(ws));
+}
+
+// signature: ((j -> Atom) -> ([j] -> [j]))
+export function filter(f) {
+  return (xs) => (
+    /* if */ equal(xs, []) ?
+    /* then */ xs :
+    /* else */ filter2(f)(xs.at(0))(xs.slice(1))
+  );
+}
+
+// signature: (a -> (a -> Atom))
+export function eq(x) {
+  return (y) => equal(x, y);
+}
+
+// signature: ([Num] -> [Num])
+export function seven(xs) {
+  return filter(eq(7.0))(xs);
 }
 
 export const pipes = [];
