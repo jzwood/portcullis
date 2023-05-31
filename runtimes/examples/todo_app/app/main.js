cf1a43fada21155b3fef1e7ef7bef660c960a517
Jake Wood <jzwood14@yahoo.com>
2023-05-31 12:02:37 -0400

    better todo_app/app/main.js

--- runtimes/examples/todo_app/app/main.js
+++ runtimes/examples/todo_app/app/main.js
@@ -3,46 +3,43 @@ import { makeGrah } from "./event_runtime.js";
 
 document.addEventListener("DOMContentLoaded", main);
 
+function main() {
+  makeGrah(pipes);
+  document.addEventListener("&todo", handleTodoEvents);
+  dispatch("&todo", []);
+}
+
+// HELPERS
+
 function removeAllChildren(elem) {
   while (elem.firstChild) {
     elem.removeChild(elem.firstChild);
   }
 }
 
-function initTodo() {
-  const initTodo = new CustomEvent("&todo", {
-    detail: [],
+function dispatch(name, data) {
+  const event = new CustomEvent(name, {
+    detail: data,
   });
-  document.dispatchEvent(initTodo);
+  document.dispatchEvent(event);
 }
 
-function initTodoListener() {
-  document.addEventListener("&todo", ({ detail: todos }) => {
-    const todoElem = document.getElementById("todo");
-    removeAllChildren(todoElem);
-    Array.from(todos).reverse().forEach((todo) => {
-      const del = document.createElement("button");
-      const X = document.createTextNode("Delete");
-      del.appendChild(X);
-      del.style.marginLeft = "1rem";
-      del.addEventListener("click", (event) => {
-        const note = event.target.previousSibling.textContent;
-        const customEvent = new CustomEvent("&done", {
-          detail: note.split(""),
-        });
-        document.dispatchEvent(customEvent);
-      });
-      const item = document.createElement("li");
-      const label = document.createTextNode(todo.join(""));
-      item.appendChild(label);
-      item.appendChild(del);
-      todoElem.appendChild(item);
+function handleTodoEvents({ detail: todos }) {
+  const todoElem = document.getElementById("todo");
+  removeAllChildren(todoElem);
+  Array.from(todos).reverse().forEach((todo) => {
+    const del = document.createElement("button");
+    const X = document.createTextNode("Delete");
+    del.appendChild(X);
+    del.style.marginLeft = "1rem";
+    del.addEventListener("click", (event) => {
+      const task = event.target.previousSibling.textContent.split("");
+      dispatch("&done", task);
     });
+    const item = document.createElement("li");
+    const label = document.createTextNode(todo.join(""));
+    item.appendChild(label);
+    item.appendChild(del);
+    todoElem.appendChild(item);
   });
-}
-
-function main() {
-  makeGrah(pipes);
-  initTodoListener();
-  initTodo();
 }
