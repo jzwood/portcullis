617cfdd98633ed4c5eb2c92701b83d403ccd0696
Jake Wood <jzwood14@yahoo.com>
2023-09-09 22:43:05 -0400

    wip

--- src/CodeGen/Po/TargetPo.hs
+++ src/CodeGen/Po/TargetPo.hs
@@ -9,7 +9,7 @@ import Data.Bifunctor
 import Data.Function
 import Control.Applicative
 import Data.Char
-import Data.List (intercalate, intersperse, nub)
+import Data.List (intercalate, intersperse, nub, dropWhileEnd)
 import Util
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
@@ -19,16 +19,25 @@ class Po ast where
 
 instance Po Module where
   toPo Module { functions, comments, streamMap, pipes }
-    = unlines $ (toPo <$> functions) ++ [zeroArityFuncs, topology]
+    = unlines [ss, ps, fns]
       where
-        zeroArityFuncs = unlines $ showZeroArityFunctions functions
-        topology = showTopology streamMap pipes
+        ss = unlines $ toPo <$> Map.elems streamMap
+        ps = unlines $ toPo <$> pipes
+        fns = unlines $ toPo <$> functions
 
 instance Po Function where
   toPo (Function name tExpr vars expr)
-    = unlines [ unwords [ name, unparen $ toPo tExpr]
-              , unwords [name, concat vars, "=", toPo expr]
+    = unlines [ unwords [ name, toPo tExpr]
+              , unwords [name, unwords vars, "="]
+              , indent $ toPo expr
               ]
+
+instance Po Pipe where
+  toPo Pipe { funcName, inStreams, outStreamName } =
+    unwords ["|", funcName, (bracket . pad. unwords) $ (\(a, b) -> unwords [a, show b]) <$> inStreams]
+
+instance Po Stream where
+  toPo Stream { streamName, streamSig } = unwords [ streamName, toPo streamSig ]
 
 instance Po TypeExpr where
   toPo NumType = "Num"
@@ -41,39 +50,35 @@ instance Po TypeExpr where
   toPo (TupType t1 t2)
     =  [t1, t2]
    <&> toPo
-    &  bracket . unwords
-  toPo (Arrow tExpr1 tExpr2) = paren (toPo tExpr1 ++ pad "->" ++ toPo tExpr2)
+    &  curly . unwords
+  toPo (Arrow tExpr1 tExpr2) = unwords ["->", toPo tExpr1, toPo tExpr2]
 
 instance Po Value where
-  toPo (Number n) = show n
+  toPo (Number n) = if fromInteger (round n) == n then show $ fromInteger $ round n else show n
   toPo (Byte b) = show b
   toPo (Atom n) = n
-  toPo (List (Unspecified "") xs) = showList $ toPo <$> xs -- so uncons displays nicely
-  toPo (List t xs) = unwords [toPo $ ListType t, showList $ toPo <$> xs]
+  toPo (List t xs) = unwords [
+    "<???>" ,
+  --toPo $ ListType t,
+    showList $ toPo <$> xs]
   toPo (Tuple e1 e2)
-    =  toPo <$> [e1, e2]
-    &  showList
+    = toPo <$> [e1, e2]
+    & curly . unwords
 
 instance Po Expr where
   toPo (Val p) = toPo p
   toPo (Ident name) = name
-  toPo (Call name exprs) = unwords [ name, concatMap toPo exprs]
+  toPo (Call name exprs) = (paren . unwords) [ name, unwords $ toPo <$> exprs]
   toPo (UnOp unop e) = toPo e ++ toPo unop
   toPo (BinOp bop e1 e2) = toPo $ Call (toPo bop) [e1, e2]
   toPo (TernOp If p e1 e2)
-    = (('\n':) . (++"\n") . indent . unwords)
-    [ toPo p ++ " ?"
-    , toPo e1 ++ " :"
-    , toPo e2
-    ]
-  toPo (TernOp Uncons xs b fb) =
-    toPo $ TernOp If (BinOp Equal xs (Val $ List (Unspecified "") [])) b (Call (toPo fb) [UnOp Head xs, UnOp Tail xs])
+    = unlines' [ unwords ["?", toPo p] , indent $ toPo e1 , toPo e2 ]
+  toPo (TernOp Uncons xs b fb)
+    = unwords [ "<+", toPo xs, toPo b, toPo fb ]
 
 instance Po UnOp where
   toPo Fst = "@1"
   toPo Snd = "@2"
-  toPo Head = "???"
-  toPo Tail = "???"
 
 instance Po Bop where
   toPo Plus = "+"
@@ -87,24 +92,3 @@ instance Po Bop where
   toPo Rem = "%"
   toPo Equal = "=="
   toPo Cons = "+>"
-
-unparen :: String -> String
-unparen = init . tail
-
-showZeroArityFunctions :: [Function] -> [String]
-showZeroArityFunctions funcs
-  =  funcs
-  &  filter (null . args)
- <&> name
- <&> (\name -> unwords ["export", "const", name, "=", '$' : name ++ "()" ])
-
-showTopology :: Map Name Stream -> [Pipe] -> String
-showTopology _ [] = "export const pipes = [];"
-showTopology streamMap pipes
-  =  pipes
- <&> showPipe streamMap
-  &  ("export const pipes = " ++) . ("[\n" ++) . (++ "\n]") . indent . intercalate ",\n"
-
-showPipe :: Map Name Stream -> Pipe -> String
-showPipe streamMap Pipe { funcName, inStreams, outStreamName } =
-  showList [funcName, showList (fmap (\(name, buffer) -> showList [show name, show buffer]) inStreams), show outStreamName]
