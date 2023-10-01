1df39a18840987974064ba99590352c92164fe9c
Jake Wood <jzwood14@yahoo.com>
2023-09-30 23:04:53 -0400

    wip

--- src/CodeGen/Html/Target.hs
+++ src/CodeGen/Html/Target.hs
@@ -20,12 +20,12 @@ import Util
 class Html ast where
   toHtml :: ast -> String
 
-escapeHtml :: String -> String
-escapeHtml content = content >>= (\case '&' -> "&amp;"; '<' -> "&lt;"; '>' -> "&gt;"; char -> [char])
+esc :: String -> String
+esc content = content >>= (\case '&' -> "&amp;"; '<' -> "&lt;"; '>' -> "&gt;"; char -> [char])
 
 tag :: String -> String -> String -> String
 tag element className content = concat [ "<", element, " class=\"", className, "\"", ">"
-                                       , escapeHtml content
+                                       , content
                                        , "</", element, ">"
                                        ]
 
@@ -40,39 +40,27 @@ instance Html Stmt where
 
 instance Html Function where
   toHtml (Function name tExpr vars expr)
-    = tag "code" "function" $ unlines' [ unwords [ name, toHtml tExpr]
+    = tag "code" "function" $ unlines' [ unwords [ name, tag "span" "type-expr" (toHtml tExpr)]
                , unwords [name, unwords vars, "="]
                , indent $ toHtml expr
                ]
 
 instance Html Pipe where
-  toHtml Pipe { funcName, inStreams, outStreamName } =
-    unwords ["|", funcName, (bracket . pad. unwords) $ (\(a, b) -> unwords [a, show b]) <$> inStreams, outStreamName]
+  toHtml pipe = tag "span" "pipe" (toPo pipe)
 
 instance Html Stream where
-  toHtml Stream { streamName, streamSig } = unwords [ streamName, toHtml streamSig ]
+  toHtml Stream { streamName, streamSig } = tag "code" "stream" $ unwords [ streamName, toHtml streamSig ]
 
 instance Html Comment where
-  toHtml (Comment comment) = concat ["#", comment, "#"]
+  toHtml comment@(Comment _) = tag "p" "comment" $ esc . toPo $ comment
 
 instance Html TypeExpr where
-  toHtml NumType = "Num"
-  toHtml ByteType = "Byte"
-  toHtml AtomType = "Atom"
-  toHtml (Unspecified t) = extractExt t
-  toHtml (ListType t)
-    = toHtml t
-    & bracket
-  toHtml (TupType t1 t2)
-    =  [t1, t2]
-   <&> toHtml
-    &  curly . unwords
-  toHtml (Arrow tExpr1 tExpr2) = unwords ["->", toHtml tExpr1, toHtml tExpr2]
+  toHtml te = esc . toPo $ te
 
 instance Html Value where
-  toHtml (Number n) = if fromInteger (round n) == n then show $ fromInteger $ round n else show n
-  toHtml (Byte b) = show b
-  toHtml (Atom n) = n
+  toHtml num@(Number _) = tag "span" "num" (toPo num)
+  toHtml byte@(Byte _) = tag "span" "byte" (toPo byte)
+  toHtml atom@(Atom _) = tag "span" "atom" (toPo atom)
   toHtml (List t xs) = concat [ toHtml t , ":" , (bracket . unwords) $ toHtml <$> xs ]
   toHtml (Tuple e1 e2)
     = toHtml <$> [e1, e2]
@@ -80,28 +68,17 @@ instance Html Value where
 
 instance Html Expr where
   toHtml (Val p) = toHtml p
-  toHtml (Ident name) = name
-  toHtml (Call name exprs) = (paren . unwords . filter (not . null)) [ name, unwords $ toHtml <$> exprs]
+  toHtml (Ident name) = tag "var" "ident" name
+  toHtml (Call name exprs) = tag "span" "call" $ (paren . unwords . filter (not . null)) [ name, unwords $ toHtml <$> exprs]
   toHtml (UnOp unop e) = unwords [ toHtml unop, toHtml e]
   toHtml (BinOp bop e1 e2) = unwords [toHtml bop, toHtml e1, toHtml e2]
   toHtml (TernOp If p e1 e2)
-    = unlines' [ unwords ["?", toHtml p] , indent $ toHtml e1 , toHtml e2 ]
+    = unlines' [ unwords [tag "span" "op" "?", toHtml p] , indent $ toHtml e1 , toHtml e2 ]
   toHtml (TernOp Uncons xs b fb)
-    = unwords [ "<+", toHtml xs, toHtml b, toHtml fb ]
+    = unwords [ tag "span" "op" (esc "<+"), toHtml xs, toHtml b, toHtml fb ]
 
 instance Html UnOp where
-  toHtml Fst = "@1"
-  toHtml Snd = "@2"
+  toHtml op = tag "span" "op" (toPo op)
 
 instance Html Bop where
-  toHtml Plus = "+"
-  toHtml Minus = "-"
-  toHtml Times = "*"
-  toHtml Divide = "/"
-  toHtml GreaterThan = ">"
-  toHtml GreaterThanOrEqual = ">="
-  toHtml LessThan = "<"
-  toHtml LessThanOrEqual = "<="
-  toHtml Rem = "%"
-  toHtml Equal = "=="
-  toHtml Cons = "+>"
+  toHtml op = tag "span" "op" (esc . toPo $ op)
