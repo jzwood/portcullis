8ca5452461f33d8d162e47d3a4b6c176041768a3
Jake Wood <jzwood14@yahoo.com>
2023-10-02 00:12:23 -0400

    html docs coming along

--- src/CodeGen/Html/Target.hs
+++ src/CodeGen/Html/Target.hs
@@ -13,7 +13,7 @@ import Data.Char
 import Data.FileEmbed (embedFile)
 import Data.Function
 import Data.Functor
-import Data.List (dropWhileEnd, groupBy, intercalate, intersperse, nub)
+import Data.List (dropWhileEnd, groupBy, intercalate, intersperse, nub, sortOn)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Prelude hiding (showList)
@@ -39,14 +39,11 @@ embody content =
 esc :: String -> String
 esc content = content >>= (\case '&' -> "&amp;"; '<' -> "&lt;"; '>' -> "&gt;"; char -> [char])
 
-tag :: String -> String -> String -> String
-tag element className content =
+tag :: String -> [(String, String)] -> String -> String
+tag element attrs content =
     concat
         [ "<"
-        , element
-        , " class=\""
-        , className
-        , "\""
+        , unwords [element, unwords $ attrs <&> \(key, val) -> concat [key, "=\"", val, "\""]]
         , ">"
         , content
         , "</"
@@ -55,7 +52,15 @@ tag element className content =
         ]
 
 instance Html Module where
-    toHtml Module{stmts} = embody $ unlines' $ toHtml <$> stmts
+    toHtml Module{stmts, functions} =
+        (embody . concat)
+            [ "<aside>"
+            , sortOn name functions >>= \fun -> tag "a" [("href", '#' : name fun)] (name fun)
+            , "</aside>"
+            , "<main>"
+            , unlines' $ toHtml <$> stmts
+            , "</main>"
+            ]
 
 instance Html Stmt where
     toHtml (F f) = toHtml f
@@ -65,29 +70,29 @@ instance Html Stmt where
 
 instance Html Function where
     toHtml (Function name tExpr vars expr) =
-        tag "code" "function" $
+        tag "code" [("id", name), ("class", "function")] $
             unlines'
-                [ unwords [tag "span" "function-name" name, toHtml tExpr]
-                , unwords [tag "span" "function-name" name, unwords $ (tag "var" "param") <$> vars, "="]
+                [ unwords [tag "span" [("class", "function-name")] name, toHtml tExpr]
+                , unwords [tag "span" [("class", "function-name")] name, unwords $ tag "var" [("class", "param")] <$> vars, "="]
                 , indent $ toHtml expr
                 ]
 
 instance Html Pipe where
-    toHtml pipe = tag "span" "pipe" (toPo pipe)
+    toHtml pipe = tag "span" [("class", "pipe")] (toPo pipe)
 
 instance Html Stream where
-    toHtml Stream{streamName, streamSig} = tag "code" "stream" $ unwords [streamName, toHtml streamSig]
+    toHtml Stream{streamName, streamSig} = tag "code" [("class", "stream")] $ unwords [streamName, toHtml streamSig]
 
 instance Html Comment where
-    toHtml comment@(Comment _) = tag "p" "comment" $ esc . toPo $ comment
+    toHtml comment@(Comment _) = tag "p" [("class", "comment")] $ esc . toPo $ comment
 
 instance Html TypeExpr where
-    toHtml te = tag "span" "type" $ esc . toPo $ te
+    toHtml te = tag "span" [("class", "type")] $ esc . toPo $ te
 
 instance Html Value where
-    toHtml num@(Number _) = tag "span" "val num" (toPo num)
-    toHtml byte@(Byte _) = tag "span" "val byte" (toPo byte)
-    toHtml atom@(Atom _) = tag "span" "val atom" (toPo atom)
+    toHtml num@(Number _) = tag "span" [("class", "val num")] (toPo num)
+    toHtml byte@(Byte _) = tag "span" [("class", "val byte")] (toPo byte)
+    toHtml atom@(Atom _) = tag "span" [("class", "val atom")] (toPo atom)
     toHtml (List t xs) = concat [toHtml t, ":", (bracket . unwords) $ toHtml <$> xs]
     toHtml (Tuple e1 e2) =
         toHtml <$> [e1, e2]
@@ -95,17 +100,17 @@ instance Html Value where
 
 instance Html Expr where
     toHtml (Val p) = toHtml p
-    toHtml (Ident name) = tag "var" "ident" name
-    toHtml (Call name exprs) = tag "span" "call" $ (paren . unwords . filter (not . null)) [tag "span" "function-name" name, unwords $ toHtml <$> exprs]
+    toHtml (Ident name) = tag "var" [("class", "ident")] name
+    toHtml (Call name exprs) = tag "span" [("class", "call")] $ (paren . unwords . filter (not . null)) [tag "span" [("class", "function-name")] name, unwords $ toHtml <$> exprs]
     toHtml (UnOp unop e) = unwords [toHtml unop, toHtml e]
     toHtml (BinOp bop e1 e2) = unwords [toHtml bop, toHtml e1, toHtml e2]
     toHtml (TernOp If p e1 e2) =
-        unlines' [unwords [tag "span" "op" "?", toHtml p], indent $ toHtml e1, toHtml e2]
+        unlines' [unwords [tag "span" [("class", "op")] "?", toHtml p], indent $ toHtml e1, toHtml e2]
     toHtml (TernOp Uncons xs b fb) =
-        unwords [tag "span" "op" (esc "<+"), toHtml xs, toHtml b, toHtml fb]
+        unwords [tag "span" [("class", "op")] (esc "<+"), toHtml xs, toHtml b, toHtml fb]
 
 instance Html UnOp where
-    toHtml op = tag "span" "op" (toPo op)
+    toHtml op = tag "span" [("class", "op")] (toPo op)
 
 instance Html Bop where
-    toHtml op = tag "span" "op" (esc . toPo $ op)
+    toHtml op = tag "span" [("class", "op")] (esc . toPo $ op)
