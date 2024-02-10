dbf019c23e082142f3bd321c38098731f9bf0877
Jake Wood <jzwood14@yahoo.com>
2024-02-09 19:31:35 -0500

    walks back some changes

--- src/CodeGen/Js/Target.hs
+++ src/CodeGen/Js/Target.hs
@@ -78,7 +78,8 @@ instance Js Expr where
     , toJs e1 ++ " :"
     , toJs e2
     ]
-  toJs (TernOp top e1 e2 e3) = toJs $ Call (toJs top) [e1, e2, e3]
+  toJs (TernOp Uncons xs b fb) =
+    toJs $ TernOp If (BinOp Equal xs (Val $ List (Unspecified "") [])) b (Call (toJs fb) [UnOp Head xs, UnOp Tail xs])
 
 instance Js UnOp where
   toJs Fst = "[0]"
@@ -98,10 +99,6 @@ instance Js Bop where
   toJs Rem = "_rem_"
   toJs Equal = "_eq_"
   toJs Cons = "_cons_"
-
-instance Js Top where
-  toJs If = "_if_"
-  toJs Uncons = "_uncons_"
 
 showAtoms :: [Function] -> [String]
 showAtoms funcs
